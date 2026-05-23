d2493d29e486bcb3d256b201e34e425a14b406ba
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2026-05-23 11:20:59 -0400

    Rough out formula for bbsclient

---
+++ Formula/bbsclient.rb
@@ -0,0 +1,23 @@
+class Bbsclient < Formula
+  desc "Terminal client for ISCA and DOC BBS systems"
+  homepage "https://github.com/StilgarISCA/IoErrorBbsClient"
+  url "https://github.com/StilgarISCA/IoErrorBbsClient/releases/download/v3.0.0-Stilgar/ioerrorbbsclient-3.0.0-Stilgar.tar.gz"
+  sha256 "TBD"
+  license "GPL-2.0-or-later"
+
+  depends_on :macos
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-release-build",
+                          "--enable-keychain",
+                          "--disable-native-optimizations"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_path_exists bin/"bbs"
+    assert_path_exists man1/"bbs.1"
+  end
+end
