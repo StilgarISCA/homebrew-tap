2da30b12872ad2e6149ec2f72dd1e1aa59caf417
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2026-05-24 09:02:14 -0400

    SHA256 for bbsclient 3.0.0-Stilgar added

--- Formula/bbsclient.rb
+++ Formula/bbsclient.rb
@@ -2,7 +2,7 @@ class Bbsclient < Formula
   desc "Terminal client for ISCA and DOC BBS systems"
   homepage "https://github.com/StilgarISCA/IoErrorBbsClient"
   url "https://github.com/StilgarISCA/IoErrorBbsClient/releases/download/v3.0.0-Stilgar/ioerrorbbsclient-3.0.0-Stilgar.tar.gz"
-  sha256 "TBD"
+  sha256 "56f53398eeea7eb0efc8a25b9f1ff5857781b72b553b97e695babc6446a04873"
   license "GPL-2.0-or-later"
 
   depends_on :macos
